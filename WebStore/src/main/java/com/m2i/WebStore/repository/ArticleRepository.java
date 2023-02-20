8ed17facde0a981418942d0b762757aeb334e06a
Irene Charles <irene.cltd@gmail.com>
2023-02-20 12:00:28 +0100

    Ajout ArticleRepository CommandLineRepository

--- WebStore/src/main/java/com/m2i/WebStore/repository/ArticleRepository.java
+++ WebStore/src/main/java/com/m2i/WebStore/repository/ArticleRepository.java
@@ -2,8 +2,8 @@ package com.m2i.WebStore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.m2i.WebStore.entity.Role;
+import com.m2i.WebStore.entity.Article;
 
-public interface ArticleRepository extends JpaRepository<Role, Integer>{
+public interface ArticleRepository extends JpaRepository<Article, Integer>{
 
 }
--- WebStore/src/main/java/com/m2i/WebStore/repository/CommandLineRepository.java
+++ WebStore/src/main/java/com/m2i/WebStore/repository/CommandLineRepository.java
@@ -2,8 +2,8 @@ package com.m2i.WebStore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.m2i.WebStore.entity.Role;
+import com.m2i.WebStore.entity.CommandLine;
 
-public interface CommandLineRepository extends JpaRepository<Role, Integer>{
+public interface CommandLineRepository extends JpaRepository<CommandLine, Integer>{
 
 }
