5ce727edcbacb1a366721651816b0a11143f1b3d
kla44 <klara.edern@orange.fr>
2023-02-20 11:30:54 +0100

    OneToMany entity Command

--- WebStore/src/main/java/com/m2i/WebStore/entity/Command.java
+++ WebStore/src/main/java/com/m2i/WebStore/entity/Command.java
@@ -1,6 +1,7 @@
 package com.m2i.WebStore.entity;
 
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,4 +31,7 @@ public class Command {
 	@ManyToOne
 	@JoinColumn(name="id_user")
 	private User user;
+	
+	@OneToMany(targetEntity = CommandLine.class, mappedBy = "command")
+	private List<CommandLine> commandLines;
 }
---
+++ WebStore/src/main/java/com/m2i/WebStore/services/CommandService.java
@@ -0,0 +1,46 @@
+package com.m2i.WebStore.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.m2i.WebStore.entity.User;
+import com.m2i.WebStore.repository.UserRepository;
+
+@Service
+public class CommandService {
+
+	@Autowired
+	UserRepository repo;
+	
+	public void create(User u) {
+		repo.save(u);
+	}
+
+	public User getById(int id) {
+		return repo.findById(id).orElse(null);
+	}
+
+	public List<User> getAll() {
+		return repo.findAll();
+	}
+
+	public void update(int id, User u) {
+		User user = repo.findById(id).orElse(null);
+		if (u!= null) {
+			user.setLogin( u.getLogin() );
+			user.setPassword( u.getPassword());
+			user.setConnectionNumber(u.getConnectionNumber());
+			repo.save(user);
+		}
+	}
+
+	public void delete(int id) {
+		User user = repo.findById(id).orElse(null);
+		if (user!= null) {
+			repo.delete(user);
+		}
+		
+	}
+}
