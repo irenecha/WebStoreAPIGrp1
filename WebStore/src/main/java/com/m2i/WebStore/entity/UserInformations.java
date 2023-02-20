903ee427afbf9c644bdf394a79dcc44b9a826e7e
Lorenzo Biscaino <lorenzo.biscaino@gmail.com>
2023-02-20 10:51:52 +0100

    modif Role

--- WebStore/src/main/java/com/m2i/WebStore/entity/UserInformations.java
+++ WebStore/src/main/java/com/m2i/WebStore/entity/UserInformations.java
@@ -1,5 +1,28 @@
 package com.m2i.WebStore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity @Table(name="user_informations")
+@Getter @Setter @NoArgsConstructor @ToString
 public class UserInformations {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idInformations;
+	
+	private String adress;
+	
+	private String city;
+	
+	private String email;
+	
+	private String phoneNumber;
 
 }
