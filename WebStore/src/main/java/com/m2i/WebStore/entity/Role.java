de77a53a42143ad5acc571cafc767d4db5ce3daa
Lorenzo Biscaino <lorenzo.biscaino@gmail.com>
2023-02-20 11:12:42 +0100

    Entity UserInformations et Role

--- WebStore/src/main/java/com/m2i/WebStore/entity/Role.java
+++ WebStore/src/main/java/com/m2i/WebStore/entity/Role.java
@@ -1,22 +1,38 @@
 package com.m2i.WebStore.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity @Table(name="role")
-@Getter @Setter @NoArgsConstructor @ToString
+@Getter @Setter @NoArgsConstructor @ToString @AllArgsConstructor
 public class Role {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idRole;
 	
 	private String RoleName;
+	
+	@ManyToMany
+    @JoinTable( name = "user_role_association",
+			    joinColumns = @JoinColumn( name = "id_role" ),
+			    inverseJoinColumns = @JoinColumn( name = "id_user" ) )
+	private List<User> users;
+
+	
+	
 
 }
