d250d2eef5cb7714932d95969f8713d50fc7dd4b
Irene Charles <irene.cltd@gmail.com>
2023-02-20 11:48:10 +0100

    modif Article et CommandLine

--- WebStore/src/main/java/com/m2i/WebStore/entity/Article.java
+++ WebStore/src/main/java/com/m2i/WebStore/entity/Article.java
@@ -1,5 +1,6 @@
 package com.m2i.WebStore.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -7,14 +8,24 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity @Table
+@Entity @Table(name = "article")
 @Getter @Setter @AllArgsConstructor @ToString
 public class Article { 
 	
+	@Column(name="id_article")
 	private int idArticle;
 	private String description;
 	private String brand;
 	private Float price;
 	
-
+	
+	public Article(String description, String brand, Float price) {
+		super();
+		this.description = description;
+		this.brand = brand;
+		this.price = price;
+	}	
+	
+	
+	
 }
