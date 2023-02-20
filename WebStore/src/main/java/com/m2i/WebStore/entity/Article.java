a640204f97a0d7e9f6bb1680c7d324eb4d7ae81d
Irene Charles <irene.cltd@gmail.com>
2023-02-20 10:14:51 +0100

    Update gitignore

---
+++ WebStore/src/main/java/com/m2i/WebStore/entity/Article.java
@@ -0,0 +1,17 @@
+package com.m2i.WebStore.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity @Table
+@Getter @Setter @AllArgsConstructor @ToString
+public class Article {
+	
+	private 
+
+}
