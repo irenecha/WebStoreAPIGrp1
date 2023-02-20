f08cd696e07c1c626ec2409847651938464cf274
Lorenzo Biscaino <lorenzo.biscaino@gmail.com>
2023-02-20 11:22:34 +0100

    fichiers Role et UserInformations Repository

---
+++ WebStore/src/main/java/com/m2i/WebStore/repository/UserInformationsRepository.java
@@ -0,0 +1,11 @@
+package com.m2i.WebStore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.m2i.WebStore.entity.UserInformations;;
+
+
+
+public interface UserInformationsRepository extends JpaRepository<UserInformations, Integer>{
+
+}
