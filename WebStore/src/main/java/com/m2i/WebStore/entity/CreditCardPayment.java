43fd10d6e2bcb554db94dee4198ea6272a4a70ed
Leandre-M <leandre.moteyo@labom2iformation.fr>
2023-02-20 14:44:25 +0100

    PaymentRepository

--- WebStore/src/main/java/com/m2i/WebStore/entity/CreditCardPayment.java
+++ WebStore/src/main/java/com/m2i/WebStore/entity/CreditCardPayment.java
@@ -1,25 +1,38 @@
 package com.m2i.WebStore.entity;
 
+import java.util.Date;
+
+import com.github.javafaker.Faker;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name="credit_card_payments")
 @PrimaryKeyJoinColumn(name="id_payment")
-@Getter @Setter @NoArgsConstructor @ToString @AllArgsConstructor
+@Getter @Setter @ToString @AllArgsConstructor
 public class CreditCardPayment extends Payment {
 		
 		@Column(name="card_number")
 		private String cardNumber;
 		
 		@Column(name="expiration_date")
-		private String dateExp;
+		private Date dateExp;
+		
+		public CreditCardPayment() {
+			super();
+			
+			Faker faker = new Faker();
+		
+			this.cardNumber = faker.toString();
+			this.dateExp = faker.date().birthday();
+			
+		}
 
 }
