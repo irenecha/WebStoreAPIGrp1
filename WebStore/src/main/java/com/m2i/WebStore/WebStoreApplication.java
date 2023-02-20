e465cd6d221acabb1425baae73e11d14cca4e8bb
Irene Charles <irene.cltd@gmail.com>
2023-02-20 09:36:31 +0100

    Init

---
+++ WebStore/src/main/java/com/m2i/WebStore/WebStoreApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.WebStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreApplication.class, args);
+	}
+
+}
