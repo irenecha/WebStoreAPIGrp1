ea9b79dfd51bc3ad10a8b4ae39d97c19a9a0c4f6
Irene Charles <irene.cltd@gmail.com>
2023-02-20 10:46:03 +0100

    ignore application.properties

--- WebStore/src/main/java/com/m2i/WebStore/entity/Article.java
+++ WebStore/src/main/java/com/m2i/WebStore/entity/Article.java
@@ -1,8 +1,7 @@
 package com.m2i.WebStore.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,8 +9,12 @@ import lombok.ToString;
 
 @Entity @Table
 @Getter @Setter @AllArgsConstructor @ToString
-public class Article {
+public class Article { 
 	
-	private 
+	private int idArticle;
+	private String description;
+	private String brand;
+	private Float price;
+	
 
 }
--- WebStore/src/main/java/com/m2i/WebStore/entity/CommandLine.java
+++ WebStore/src/main/java/com/m2i/WebStore/entity/CommandLine.java
@@ -1,5 +1,14 @@
 package com.m2i.WebStore.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class CommandLine {
+	
+	@Id @GeneratedValue( strategy = GenerationType.IDENTITY)
+	private int id;
+	private int quantity;
+	
 
 }
